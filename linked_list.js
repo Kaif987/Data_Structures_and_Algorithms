480ba7d7581fa9dcf08c7dbe25a994565fe1ae0f
Kaif Siddiqui <mdkaifprofession@gmail.com>
2022-10-26 22:06:29 +0530

    Insert and remove method added to linked list

--- linked_list.js
+++ linked_list.js
@@ -19,13 +19,85 @@ class LinkedList {
             this.head = newNode
         }else{
             current = this.head
-            while(current){
-                current = 
+            while(current.next){
+                current = current.next
             }
+            current.next = newNode
         }
+        this.size++
     }
-    
+
+    insertAtNode(element, index){
+        if(index < 0 || index > this.size){
+            console.log("Enter a valid index")
+            return 
+        }
+        const node = new Node(element)
+        let current, prev;
+        
+        if(index === 0){
+            node.next = this.head
+            this.head = node
+        }else{
+            let it = 0
+            current = this.head
+            while(it < index){
+                it++
+                prev = current
+                current = current.next
+            }
+            prev.next = node
+            node.next = current
+        }
+        this.size++
+    }
+
+    // remove from index
+    removeAtIndex(index){
+        if(index < 0 || index >= this.size){
+            console.log("Please enter a valid index")
+        }
+        let prev, curr;
+        curr = this.head
+        prev = curr
+
+        if(index === 0){
+            this.head = curr.next
+        }else{
+            let it = 0
+            while(it < index){
+                curr = curr.next
+                prev = curr
+            }
+            prev.next = current.next
+        }
+        this.size--
+    }
+    removeElement(element){
+        let curr = this.head
+        let prev = null;
+        while(curr !== null){
+            if(curr.element === element){
+                if(prev === null){
+                    this.head = null
+                }else{
+                    prev.next = curr.next
+                }
+                this.size--
+                return curr.element
+            }
+            prev = curr;
+            curr = curr.next
+        }
+        return -1
+    }
 }
 
-const firstNode = new Node(12)
-console.log(firstNode)
+const linkedList = new LinkedList()
+linkedList.add(24)
+linkedList.add(33)
+linkedList.insertAtNode(53, 2)
+linkedList.removeElement(24)
+
+
+console.log(linkedList)
